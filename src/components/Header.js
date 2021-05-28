427da3c826ae8623d8fb4c8ce347331fd95b15d0
Euntae Kim <hurima90@gmail.com>
2021-05-29 00:20:32 +0900

    Feat(Header): add mobile menu(hambuger button) feature

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,35 +1,60 @@
-import React from 'react'
-import { Nav, NavLink, Bars, NavMenu, NavBtn, NavBtnLink } from '../styles/HeaderElements'
+import React, { useEffect, useState } from 'react'
+import { Nav, NavLink, NavLogo, NavItem, MobileIcon, NavMenu, NavBtnLink, NavbarContainer } from '../styles/HeaderElements'
 import logo from '../assets/Team28-logo.png'
-import { FaSearch } from 'react-icons/fa'
+import { FaSearch, FaTimes, FaBars } from 'react-icons/fa'
+import { IconContext } from 'react-icons/lib'
 
 const Header = () => {
+    const [click, setClick] = useState(false)
+    const [scroll, setScroll] = useState(false)
+
+    const handleClick = () => setClick(!click)
+    const closeMobileMenu = () => setClick(false)
+
+    const changeNav = () => {
+        if(window.scrollY >= 80){
+            setScroll(true)
+        } else {
+            setScroll(false)
+        }
+    }
+
+    useEffect(() => {
+        changeNav()
+        window.addEventListener('scroll', changeNav)
+    }, [])
+
     return (
         <>
-            <Nav>
-                <NavLink to="/">
-                    <img src={logo} alt="logo" width="150px"/>
-                </NavLink>
-                <Bars />
-                <NavMenu>
-                    <NavLink to="/" activeStyle>
-                        Home
-                    </NavLink>
-                    <NavLink to="/contact-us" activeStyle>
-                        Contact Us
-                    </NavLink>
-                    <NavLink to="/sign-up" activeStyle>
-                        Sign Up
-                    </NavLink>
-                    <NavLink to="/search" activeStyle>
-                        <FaSearch />
-                    </NavLink>
-                    <NavBtnLink to='/sign-in'>Sign In</NavBtnLink>
-                </NavMenu>
-                {/* <NavBtn>
-                    <NavBtnLink to='/signin'>Sign In</NavBtnLink>
-                </NavBtn> */}
+        <IconContext.Provider value={{ color: '#131313' }}>
+            <Nav active={scroll} click={click}>
+                <NavbarContainer>
+                    <NavLogo to="/" onClick={closeMobileMenu}>
+                        <img src={logo} alt="logo" width="150px"/>
+                    </NavLogo>
+                    <MobileIcon onClick={handleClick}>
+                        {click ? <FaTimes /> : <FaBars />}
+                    </MobileIcon>
+                    <NavMenu onClick={handleClick} click={click}>
+                        <NavItem>
+                            <NavLink to="/" activeStyle>Home</NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink to="/contact-us" activeStyle>Contact Us</NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink to="/sign-up" activeStyle>Sign Up</NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink to="/search" activeStyle><FaSearch /></NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavBtnLink to="/sign-in" activeStyle>Sign In</NavBtnLink>
+                        </NavItem>
+                    </NavMenu>
+                </NavbarContainer>
             </Nav>
+        </IconContext.Provider>
         </>
     )
 }
--- src/styles/HeaderElements.js
+++ src/styles/HeaderElements.js
@@ -1,6 +1,5 @@
 import styled from 'styled-components'
 import { NavLink as Link } from 'react-router-dom'
-import { FaBars } from 'react-icons/fa'
 
 export const Nav = styled.nav`
     background: #fff;
@@ -12,7 +11,15 @@ export const Nav = styled.nav`
     border-bottom: 1px solid #ececec;
 `
 
-export const NavLink = styled(Link)`
+export const NavbarContainer = styled.div`
+    display: flex;
+    justify-content: space-between;
+    height: 80px;
+    z-index: 1;
+    width: 100%;
+`
+
+export const NavLogo = styled(Link)`
     color: #000;
     display: flex;
     align-items: center;
@@ -26,14 +33,14 @@ export const NavLink = styled(Link)`
     }
 `
 
-export const Bars = styled(FaBars)`
+export const MobileIcon = styled.div`
     display: none;
     color: #000;
 
     @media screen and (max-width: 768px){
         display: block;
         position: absolute;
-        top: 0;
+        top: -15px;
         right: 0;
         transform: translate(-100%, 75%);
         font-size: 1.8rem;
@@ -41,29 +48,61 @@ export const Bars = styled(FaBars)`
     }
 `
 
-export const NavMenu = styled.div`
+export const NavMenu = styled.ul`
     display: flex;
     align-items: center;
-    margin-right: -24px;
-
     margin-right: 24px;
+    margin-bottom: 0;
+    list-style: none;
+    text-align: center;
 
     @media screen and (max-width: 768px){
-        display: none;
+        flex-direction: column;
+        width: 100%;
+        height: 90vh;
+        position: absolute;
+        top: 80px;
+        top: ${({ click }) => (click ? "100" : "-1000px")};
+        opacity: 1;
+        transition: all 0.2s ease;
+        background: #fff;
+        z-index: 999;
+        padding-left: 0;
     }
 `
 
-export const NavBtn = styled.nav`
-    display: flex;
-    align-items: center;
-    margin-right: 24px;
+export const NavItem = styled.li`
+    height: 80px;
 
     @media screen and (max-width: 768px){
-        display: none;
+        width: 100%;
+    }
+`
+
+export const NavLink = styled(Link)`
+    color: #000;
+    display: flex;
+    align-items: center;
+    text-decoration: none;
+    padding: 0.5rem 1rem;
+    height: 100%;
+
+    @media screen and (max-width: 768px){
+        text-align: center;
+        padding: 2rem;
+        width: 100%;
+        display: table;
+
+        &:hover {
+            color: #256ce1;
+            transition: all 0.3s ease;
+        }
     }
 `
 
 export const NavBtnLink = styled(Link)`
+    display: flex;
+    align-items: center;
     border-radius: 40px;
     background: transparent;
     border: 1px solid #256ce1;
@@ -75,10 +114,27 @@ export const NavBtnLink = styled(Link)`
     text-decoration: none;
 
     margin-left: 24px;
+    margin-top: 20px;
 
     &:hover{
         transition: all 0.2s ease-in-out;
         background: #256ce1;
         color: #fff;
+    }
+
+    @media screen and (max-width: 768px){
+        text-align: center;
+        padding: 2rem;
+        width: 100%;
+        display: table;
+        border-radius: 0;
+        margin-left: 0;
+        vertical-align: bottom;
+
+        &:hover{
+            transition: all 0.2s ease-in-out;
+            background: #256ce1;
+            color: #fff;
+        }
     }
 `
