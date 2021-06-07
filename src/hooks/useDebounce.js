23ab1363c75b8db44a3c8f72269e74c935e834b5
supin <qorgkr26@sookmyung.ac.kr>
2021-06-07 15:46:04 +0900

    chore : useMemo 주석처리

--- src/hooks/useDebounce.js
+++ src/hooks/useDebounce.js
@@ -1,12 +1,8 @@
-import { useEffect, useMemo, useState } from 'react';
-import hashtagAPI from 'api/hashtagapi';
+import { useEffect, useState } from "react";
+import hashtagAPI from "api/hashtagapi";
 
 function useDebounce(lastCall, propsHash) {
   let [timer, setTimer] = useState(lastCall);
-  /*const hashtag = useMemo(
-    () => hashtagAPI.filterPostByHashtag(propsHash),
-    [propsHash]
-  );*/ //만약 다른 해시태그를 누르면 무조건 반응하게 하고 싶다면 useMemo를 적용해서 렌더링 해도 좋을 것 같음.
   const delay = 1000; //1s
 
   useEffect(() => {
@@ -24,3 +20,8 @@ function useDebounce(lastCall, propsHash) {
 }
 
 export default useDebounce;
+
+/*const hashtag = useMemo(
+    () => hashtagAPI.filterPostByHashtag(propsHash),
+    [propsHash]
+  );*/ //만약 다른 해시태그를 누르면 무조건 반응하게 하고 싶다면 useMemo를 적용해서 렌더링 해도 좋을 것 같음.
