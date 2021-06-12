911452a08f0cfea5270d70fec33a8749910e3f01
supin <qorgkr26@sookmyung.ac.kr>
2021-06-12 22:59:12 +0900

    feat : 댓글 db get & post & delete 기능 구현

--- src/components/CommentUser.js
+++ src/components/CommentUser.js
@@ -1,29 +1,46 @@
-import React from "react";
+import CommentsAPI from "api/CommentsAPI";
+import React, { useEffect, useState } from "react";
 import store from "store/store";
-import { ID, Date, EditBtn, DeleteBtn, btns } from "styles/CommentElements";
+import {
+  commentTexts,
+  Thumbnail,
+  ID,
+  Date,
+  EditBtn,
+  DeleteBtn,
+  btns,
+} from "styles/CommentElements";
+import CommentTexts from "./CommentTexts";
 
-const CommentTexts = ({ id, date }) => {
+const CommentUser = ({ id, date, thumbnail, refs, texts }) => {
+  const [userName, setUserName] = useState(null);
+
   const {
     userInfo: { name },
   } = store.getState();
 
+  useEffect(() => {
+    const res = CommentsAPI.getCommentUserID(id);
+  }, []);
+
   return (
-    <div>
-      <ID>{id}</ID>
-      <Date>
-        {date
-          .trim()
-          .split("-")
-          .map((d) => `${d}.`)}
-      </Date>
-      {id === name && (
-        <div style={btns}>
-          <EditBtn>수정</EditBtn>
-          <DeleteBtn>삭제</DeleteBtn>
+    <>
+      <Thumbnail img={thumbnail}></Thumbnail>
+      <div style={commentTexts}>
+        <div>
+          <ID>{id}</ID>
+          <Date>{date}</Date>
+          {id === name && (
+            <div style={btns}>
+              <EditBtn>수정</EditBtn>
+              <DeleteBtn>삭제</DeleteBtn>
+            </div>
+          )}
         </div>
-      )}
-    </div>
+        <CommentTexts ref={refs} texts={texts}></CommentTexts>
+      </div>
+    </>
   );
 };
 
-export default CommentTexts;
+export default CommentUser;
