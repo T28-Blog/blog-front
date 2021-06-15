44811886785f114e170556184687537e689905c7
Yoojeong Lee <yjclarelee@postech.ac.kr>
2021-06-15 13:59:49 +0900

    Feat: Show email and password error message on signup and signin

--- src/components/FormLib.jsx
+++ src/components/FormLib.jsx
@@ -1,36 +1,45 @@
-import { useField } from 'formik'
+import { useField } from "formik";
 import {
-    StyledTextInput,
-    StyledLabel,
-    ErrorMsg
-} from '../styles/SignElements.js'
+  StyledTextInput,
+  StyledLabel,
+  ErrorMsg,
+} from "../styles/SignElements.js";
 
-export const TextInput = ({...props}) => {
-    const [field, meta] = useField(props)
+export const TextInput = ({ ...props }) => {
+  const [field, meta] = useField(props);
+  const {
+    isExistingEmail,
+    isInvalidEmail,
+    isInvalidPassword,
+  } = props;
+  return (
+    <div>
+      <StyledLabel htmlFor={props.name}>{props.label}</StyledLabel>
 
-    return (
-        <div>
-            <StyledLabel htmlFor={props.name}>{props.label}</StyledLabel>
+      {props.type !== "password" && (
+        <StyledTextInput
+          invalid={meta.touched && meta.error}
+          {...field}
+          {...props}
+        />
+      )}
+      {isExistingEmail && <ErrorMsg>Email exists</ErrorMsg>}
+      {isInvalidEmail && <ErrorMsg>Email does not exist</ErrorMsg>}
 
-            {props.type !== "password" && (
-                <StyledTextInput invalid={meta.touched && meta.error}
-                    {...field}
-                    {...props} 
-                />
-            )}
+      {props.type === "password" && (
+        <StyledTextInput
+          invalid={meta.touched && meta.error}
+          {...field}
+          {...props}
+        />
+      )}
+      {isInvalidPassword && <ErrorMsg>Password is not valid</ErrorMsg>}
 
-            {props.type === "password" && (
-                <StyledTextInput invalid={meta.touched && meta.error}
-                    {...field}
-                    {...props} 
-                />
-            )}
-
-            {meta.touched && meta.error ? (
-                <ErrorMsg>{meta.error}</ErrorMsg>
-            ):(
-                <ErrorMsg style={{visibility: "hidden"}}>.</ErrorMsg>
-            )}
-        </div>
-    )
-}
+      {meta.touched && meta.error ? (
+        <ErrorMsg>{meta.error}</ErrorMsg>
+      ) : (
+        <ErrorMsg style={{ visibility: "hidden" }}>.</ErrorMsg>
+      )}
+    </div>
+  );
+};
