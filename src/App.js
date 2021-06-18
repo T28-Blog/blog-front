091a4f1ff98622333be71373452cbf00752dd3e6
Yoojeong Lee <yjclarelee@postech.ac.kr>
2021-06-18 14:05:04 +0900

    Edit: airpalne to airplane and Singup to Signup

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ import WritePost from "pages/WritePost";
 import Footer from "./components/Footer";
 import Comments from "components/Comments";
 import Error from "pages/Error";
-import SuccessSingUp from "./pages/SuccessSignUp";
+import SuccessSignUp from "./pages/SuccessSignUp";
 import SuccessLogout from "./pages/SuccessLogout";
 import UserInfo from "pages/UsersInfo";
 import PreLoader from "pages/PreLoader";
@@ -31,7 +31,7 @@ function App() {
         <Route path="/search" exact component={Search} />
         <Route path="/sign-up" exact component={SignUp} />
         <Route path="/comment" exact component={Comments} />
-        <Route path="/success" exact component={SuccessSingUp} />
+        <Route path="/success" exact component={SuccessSignUp} />
         <Route path="/logout" exact component={SuccessLogout} />
         <Route path="/userinfo" exact component={UserInfo} />
         <Route path="/loading" exact component={PreLoader} />
