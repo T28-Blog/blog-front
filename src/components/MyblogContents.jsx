244e6b299988d3a8ded9fb4ee9f58ca5db73752a
Euntae Kim <hurima90@gmail.com>
2021-06-21 00:23:55 +0900

    Feat: 프로필 수정 모달창 구현(닫기버튼, esc닫기, 창외부클릭시 닫기 기능)

--- src/components/MyblogContents.jsx
+++ src/components/MyblogContents.jsx
@@ -1,4 +1,6 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useState, useCallback } from 'react';
+import { useEffect } from 'react';
 import {
     MyPostContainer,
     MyPostContents,
@@ -7,21 +9,69 @@ import {
     MyPostDesc,
     MyPostDate,
     MyPostThumbnail
-} from 'styles/MyBlogElements'
-import JSONDATA from "../assets/MOCK_DATA.json"
+} from 'styles/MyBlogElements';
+import MOCK from '../assets/MOCK_DATA.json'
 
 const MyblogContents = () => {
+
+    const [result, setResult] = useState([]);
+    const [item, setItem] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+
+    const fetchMoreData = async() => {
+        setIsLoading(true);
+        setResult(result.concat(item.slice(0, 10)));
+        setItem(item.slice(10));
+        setIsLoading(false);
+    }
+
+    const infiniteScroll = useCallback(() => {
+        let scrollHeight = Math.max(document.documentElement.scrollHeight, document.body.scrollHeight);
+        let scrollTop = Math.max(document.documentElement.scrollTop, document.body.scrollTop);
+        let clientHeight = document.documentElement.clientHeight;
+
+        scrollHeight -= 100;
+
+        if(scrollTop + clientHeight >= scrollHeight && isLoading === false){
+            fetchMoreData();
+        }
+    }, [isLoading]);
+
+    const getFetchData = async() => {
+        await axios.get("https://jsonplaceholder.typicode.com/posts")
+            .then((res) => {
+                let response = res.data;
+                console.log(res)
+                setResult(response.slice(0, 10));
+                response = response.slice(10);
+                setItem(response);
+                setIsLoading(false);
+            })
+            .catch(error => {
+                return console.log(error);
+            });
+    }
+
+    useEffect(() => {
+        getFetchData();
+    }, []);
+
+    useEffect(() => {
+        window.addEventListener('scroll', infiniteScroll, true);
+        return () => window.removeEventListener('scroll', infiniteScroll, true);
+    }, [infiniteScroll]);
+
     return (
         <MyPostContainer>
             {
-                JSONDATA.map((val, key) => (
-                    <MyPostContents key={val.id}>
+                item.map((post, idx)  => (
+                    <MyPostContents key={idx}>
                         <MyPostContent>
-                            <MyPostTitle>{val.title}</MyPostTitle>
-                            <MyPostDesc>{val.desc}</MyPostDesc>
-                            <MyPostDate>20201.06.17</MyPostDate>
+                            <MyPostTitle>{post.title}</MyPostTitle>
+                            <MyPostDesc>{post.body}</MyPostDesc>
+                            <MyPostDate>{post.date}</MyPostDate>
                         </MyPostContent>
-                        <MyPostThumbnail img src={val.image} alt='thumbnail'></MyPostThumbnail>
+                        <MyPostThumbnail img src={post.image} alt='thumbnail'></MyPostThumbnail>
                     </MyPostContents>
                 ))
             }
