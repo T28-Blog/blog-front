c51a8d628786cccc4ab42ca5409834826367b811
Yoojeong Lee <yjclarelee@postech.ac.kr>
2021-06-21 09:02:17 +0900

    Edit: Branch condition depending on existence of hashtag

--- server/Posts.js
+++ server/Posts.js
@@ -22,28 +22,50 @@ app.post("/", (req, res, next) => {
   } = req.query;
   //post가 없는 경우 새로 생성
   if (post_id) {
-    console.log("posting");
-    db.database()
-      .ref(`posts/post_${post_id}`)
-      .set({
-        content,
-        date,
-        desc,
-        hashtag,
-        hits,
-        img,
-        name,
-        post_id,
-        title,
-        user_id,
-      })
-      .then((response) => {
-        console.log("done");
-        res.json({ isSaved: true });
-      })
-      .catch((err) => {
-        res.json({ isSaved: false, messaage: err });
-      });
+    if (hashtag) {
+      db.database()
+        .ref(`posts/post_${post_id}`)
+        .set({
+          content,
+          date,
+          desc,
+          hashtag,
+          hits,
+          img,
+          name,
+          post_id,
+          title,
+          user_id,
+        })
+        .then((response) => {
+          console.log("done");
+          res.json({ isSaved: true });
+        })
+        .catch((err) => {
+          res.json({ isSaved: false, messaage: err });
+        });
+    } else {
+      db.database()
+        .ref(`posts/post_${post_id}`)
+        .set({
+          content,
+          date,
+          desc,
+          hits,
+          img,
+          name,
+          post_id,
+          title,
+          user_id,
+        })
+        .then((response) => {
+          console.log("done");
+          res.json({ isSaved: true });
+        })
+        .catch((err) => {
+          res.json({ isSaved: false, messaage: err });
+        });
+    }
   }
 });
 
