9ac79cd367d459711a3056bea714d1f0a4a5ca79
supin <qorgkr26@sookmyung.ac.kr>
2021-09-30 19:59:43 +0900

    [BLOG-VER2-67] feat : CustomRouter 설정

--- src/App.js
+++ src/App.js
@@ -2,14 +2,31 @@ import Main from 'pages/Main';
 import SignUp from 'pages/SignUp';
 import SignIn from 'pages/SignIn';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { auth, onAuthStateChanged } from './api/Firebase';
+import { useEffect, useState } from 'react';
+import CustomRouter from './CustomRouter';
 
 function App() {
+  const [isUser, setUser] = useState(false);
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        //log-In
+        setUser(true);
+      } else {
+        //log-Out
+        setUser(false);
+      }
+    });
+  }, []);
+
   return (
     <Router>
       <Switch>
         <Route path="/" exact component={Main} />
-        <Route path="/sign-in" component={SignIn} />
-        <Route path="/sign-up" component={SignUp} />
+        <CustomRouter path="/sign-in" user={isUser} component={SignIn} />
+        <CustomRouter path="/sign-up" user={isUser} component={SignUp} />
       </Switch>
     </Router>
   );
