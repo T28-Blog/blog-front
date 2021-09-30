95ae57926f478f632433a439a409f32149e22512
supin <qorgkr26@sookmyung.ac.kr>
2021-09-30 18:16:09 +0900

    [BLOG-VER2-67] feat : 회원가입 & 로그인 폼 validation 수정

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,12 +1,26 @@
 import FormSignUp from 'components/form/signup/FormSignUp';
 import FormSuccess from 'components/form/FormSuccess';
 import React, { useState } from 'react';
+import authentication from 'api/Authentication';
 
 const SignUp = () => {
   const [isSubmitted, setIsSubmitted] = useState(false);
 
-  function submitForm() {
-    setIsSubmitted(true);
+  function submitForm(userInfo) {
+    const { username, email, password } = userInfo;
+    const { createUserInFirebaseAuth, updateUserProfile } = authentication;
+    createUserInFirebaseAuth(email, password).then(({ error, user, code }) => {
+      if (error) {
+        //error 발생
+      } else {
+        updateUserProfile({ displayName: username }).then(({ error }) => {
+          if (error) {
+          } else {
+            setIsSubmitted(true);
+          }
+        });
+      }
+    });
   }
   return (
     <>
