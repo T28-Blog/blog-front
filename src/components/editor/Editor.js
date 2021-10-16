75d5459ebeedc4ee93705a968f8359702be665f0
hurima90-kim <hurima90@gmail.com>
2021-10-16 23:00:44 +0900

    [#77] feat: 게시글 작성 페이지 UI 구현(타이틀,썸네일,내용,버튼) 및 타이틀 상태저장(DB저장X) 기능 구현

--- src/components/editor/Editor.js
+++ src/components/editor/Editor.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { CKEditor } from '@ckeditor/ckeditor5-react';
 import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 
--- src/pages/post/WritePost.js
+++ src/pages/post/WritePost.js
@@ -1,8 +1,58 @@
 import Editor from 'components/editor/Editor';
-import React from 'react';
+import React, { useRef, useState } from 'react';
+import { style } from './WritePostStyle';
+import thumbnail from 'assets/1.jpeg';
 
 const WritePost = () => {
-  return <Editor />;
+  const [content, setContent] = useState({
+    title: '',
+    content: '',
+  });
+  const inputOpenImageRef = useRef(null);
+
+  const getTitle = (e) => {
+    const { name, value } = e.target;
+    setContent({
+      ...content,
+      [name]: value,
+    });
+    console.log(content);
+  };
+
+  const handleOpenImageRef = () => {
+    inputOpenImageRef.current.click();
+  };
+
+  const handleUploadImage = (e) => {
+    const file = e.target.files[0];
+  };
+
+  return (
+    <WriteContainer>
+      <ButtonContainer>
+        <Button>뒤로</Button>
+        <Button>저장</Button>
+      </ButtonContainer>
+      <Title
+        type="text"
+        name="title"
+        placeholder="제목을 입력하세요."
+        onChange={getTitle}
+      />
+      <Thumbnail>
+        <img src={thumbnail} alt="thumbnail" onClick={handleOpenImageRef} />
+        <input
+          type="file"
+          accept="image.jpeg, image/png"
+          onChange={handleUploadImage}
+          ref={inputOpenImageRef}
+        />
+      </Thumbnail>
+      <Editor />
+    </WriteContainer>
+  );
 };
 
 export default WritePost;
+
+const { WriteContainer, Title, Thumbnail, ButtonContainer, Button } = style;
